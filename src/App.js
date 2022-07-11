e548e9c69e640fdf5ed3779ca02c4c08d6f3a273
Lymanto Hadibrata <lymantohadibrata@192.168.1.3>
2022-07-11 18:16:17 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import {SafeAreaView, StatusBar, Text} from 'react-native';
+import {SplashScreen} from './pages';
+
+const App = () => {
+  return <SplashScreen />;
+};
+
+export default App;
